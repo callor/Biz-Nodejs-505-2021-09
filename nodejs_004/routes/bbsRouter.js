8a3d95d507666dae5947520a62ae394a3b287514
callor <callor@callor.com>
2021-08-23 17:45:24 +0900

    bbs

--- nodejs_004/routes/bbsRouter.js
+++ nodejs_004/routes/bbsRouter.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { tbl_bbs } = require("../models/index");
+const moment = require("moment");
 
 router.get("/write", (req, res) => {
-  res.render("write");
+  const BBS = {
+    b_date: moment().format("YYYY[-]MM[-]DD"),
+    b_time: moment().format("HH:mm:ss"),
+  };
+  res.render("write", { BBS });
 });
 
 router.post("/write", (req, res) => {
@@ -15,6 +20,7 @@ router.post("/write", (req, res) => {
 
 router.get("/update", (req, res) => {
   const b_id = req.query.b_id;
+  // tbl_bbs.findOne({ where: { b_id } }).then((result) => {
   tbl_bbs.findByPk(b_id).then((result) => {
     res.render("write", { BBS: result });
   });
