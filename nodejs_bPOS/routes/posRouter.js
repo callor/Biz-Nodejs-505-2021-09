d6a67c98f7b8bc57d98d169e51ff7c995f4f2d6f
callor <callor@callor.com>
2021-08-31 11:50:40 +0900

    Pos_Order

--- nodejs_bPOS/routes/posRouter.js
+++ nodejs_bPOS/routes/posRouter.js
@@ -12,4 +12,17 @@ router.get("/order/:table_id", (req, res) => {
   res.render("order_view", { table_id });
 });
 
+router.get("/order/:table_id/input/:menu_id", (req, res) => {
+  const table_id = req.params.table_id;
+  const menu_id = req.params.menu_id;
+
+  const menu = {
+    table_id,
+    menu_id,
+    menu_name: "1000원김밥",
+    menu_price: 1000,
+  };
+  res.json(menu);
+});
+
 module.exports = router;
