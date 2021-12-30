707249ac59198a4bfff4afcd75910482a89289db
callor <callor@callor.com>
2021-12-30 16:26:12 +0900

    Passport

--- nodejs_900_passport/modules/member.js
+++ nodejs_900_passport/modules/member.js
@@ -4,17 +4,21 @@ const member_file = "./config/member.json";
 export const member_append = (join_member) => {
     const members = member_read();
 
-    const find_member = members.find((member) => member.userid === join_member.userid);
+    const find_member = members.find((member) => member.username === join_member.username);
     let new_members = [];
+
+    // 기존에 있는 memebr 이면 update
     if (find_member)
         new_members = [
             ...members.map((member) => {
-                if (member.userid === join_member.userid) return join_member;
+                if (member.username === join_member.username) return join_member;
                 else member;
             }),
         ];
+    // 없는 member 이면 추가
     else new_members = [...members, join_member];
 
+    // 파일에 저장
     fs.writeFileSync(member_file, JSON.stringify(new_members));
 };
 
